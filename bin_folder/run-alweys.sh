1bff0f40e6efe1a1c2c7cafc44c84388f1695f1e
Konstantin Kakunin (Datana) <kakunin@lanit.ru>
2020-08-18 14:54:52 +0300

    fix от проекта https://github.com/deviantony/docker-elk

--- bin_folder/run-alweys.sh
+++ bin_folder/run-alweys.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-docker run --restart=always docker-elk-master_elasticsearch &
-docker run --restart=always wurstmeister/zookeeper &
-docker run --restart=always wurstmeister/kafka &
-docker run --restart=always docker-elk-master_kibana &
-docker run --restart=always docker-elk-master_logstash &
+#docker run --restart=always docker-elk-master_elasticsearch &
+#docker run --restart=always wurstmeister/zookeeper &
+#docker run --restart=always wurstmeister/kafka &
+#docker run --restart=always docker-elk-master_kibana &
+#docker run --restart=always docker-elk-master_logstash &
+docker update --restart unless-stopped $(docker ps -q)
