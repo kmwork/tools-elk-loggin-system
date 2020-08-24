496bfc58876b8d6b436fb4adb35f783cecf27742
Konstantin Kakunin (Datana) <kakunin@lanit.ru>
2020-08-24 15:44:14 +0300

    fix logstash.conf для Кафки сделано по задаче: https://jira.dds.lanit.ru/browse/NKR-593

--- bin_folder/run-alweys.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#docker run --restart=always docker-elk-master_elasticsearch &
-#docker run --restart=always wurstmeister/zookeeper &
-#docker run --restart=always wurstmeister/kafka &
-#docker run --restart=always docker-elk-master_kibana &
-#docker run --restart=always docker-elk-master_logstash &
-docker update --restart unless-stopped $(docker ps -q)
